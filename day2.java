ced8bc47d6a2f9a7645b06ac04b648de804b5cac
Monty1223 <montydehariya77@gmail.com>
2025-04-17 13:07:47 +0530

    Add files via upload

---
+++ day2.java
@@ -0,0 +1,29 @@
+public class day2{
+    public static void main(String[] args){
+    int a=123;
+    int sum=0;
+    while (a>0) { 
+        sum=sum+ a%10;
+        a=a/10;
+    }
+    System.out.println( sum);
+
+}
+
+}
+
+// res arr print
+//  public class day2{
+//    public static void main(String[] args){
+//         int[] arr={1,2,3,4,5,6};
+//     for(int i=5;i<arr.length;i--){
+//            System.out.println(arr[i]);
+//        }
+
+
+
+        
+//     }
+//  }
+// plendrone number
+// plendrone number
