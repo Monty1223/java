cd5ec9c97b6d4c6c352670a462c12953e4199dfd
Monty1223 <montydehariya77@gmail.com>
2025-04-28 23:13:53 +0530

    Add files via upload

---
+++ j_h1.java
@@ -0,0 +1,37 @@
+// public class j_h1 {
+//     public static void main(String[] args) {
+//         int n=12345;
+//         int sum=0;
+//         while(n!=0){
+//         int temp= n%10;
+//         n=n/10;
+        
+//         sum=sum+temp;
+
+        
+            
+//         } System.out.println(sum);
+        
+//     }
+    
+// }
+
+
+
+public class j_h1 {
+    public static void main(String[] args) {
+        int n=12345;
+        int product=1;
+        while(n!=0){
+        int temp= n%10;
+        n=n/10;
+        
+        product=product*temp;
+
+        
+            
+        } System.out.println(product);
+        
+    }
+    
+}
