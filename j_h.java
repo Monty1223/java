59209011adc83e72a28d58f61f0e035fc2f15565
Monty1223 <montydehariya77@gmail.com>
2025-04-28 22:54:19 +0530

    Add files via upload

---
+++ j_h.java
@@ -0,0 +1,38 @@
+// public class j_h {
+//     public static void main(String[] args) {
+//         Scanner sc =new Scanner(System.in);
+//         System.out.println("enter value of n:");
+//          int n = sc.nextInt();
+//         int n=9;
+//         for (int i = 1; i <=n; i++) {
+//             System.out.println(i);
+            
+//         }
+
+
+//     }
+    
+// }
+// all even number between the range
+// public class j_h {
+//     public static void main(String[] args) {
+//         int n =30;
+//         for(int i=1;i<=n;i++){
+//             if(i%2==0){
+//                 System.out.println(i);
+//             }
+//         }
+//     }
+
+
+// }
+    // N NATURAL NUMBER IN REVERS ORDER
+    public class j_h {
+        public static void main(String[] args) {
+            int n=30;
+            for (int i = n; i>0 ; i--) {
+                System.out.println(i);
+                
+            }
+        }
+    }
