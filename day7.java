87ba15df00a59f8b98f1bda23e7b83cd380c63ca
Monty1223 <montydehariya77@gmail.com>
2025-04-28 13:59:26 +0530

    Add files via upload

---
+++ day7.java
@@ -0,0 +1,17 @@
+public class day7 {
+    
+        // static void sum (int a){
+        //     System.out.println(a);
+        void sum( int a){
+            System.out.println(a);
+        }
+
+
+        
+       public static void main(String[] args){
+        day7 obj=new day7();
+        obj. sum (5);
+
+    }
+    
+}
