1d46bf9bf460c381a46d17a9310e6bc99617c440
Monty1223 <montydehariya77@gmail.com>
2025-05-01 11:28:35 +0530

    Add files via upload

---
+++ day8.java
@@ -0,0 +1,109 @@
+
+
+
+// public class day8 {
+//     public static void main(String[] args) {
+//         int target=5;
+//         int arr[]={1,2,3,4,5,6,7,8,9};
+//         int startpoint=0;
+//         int endpoint=arr.length-1;
+//         while (startpoint<=endpoint) { 
+//             int midpoint=(startpoint+endpoint)/2;
+//             if(arr[midpoint]==target){
+//                 System.out.println("at index:"+midpoint);}
+//             if (arr[midpoint]>target) {
+//                    endpoint=arr[midpoint-1];
+                
+//             } else {
+//                 startpoint=arr[midpoint+1];
+//             }
+            
+
+            
+//         }
+//     }
+
+// }
+// public class day8 {
+//     public static void main(String[] args) {
+//         Scanner sc =new Scanner(System.in);
+    
+//         int target=sc.nextInt();
+//         int arr[]={1,2,3,4,5,6,7,8,9};
+//         int startpoint=0;
+//         int endpoint=arr.length-1;
+//         while (startpoint<=endpoint) { 
+//             int midpoint=(startpoint+endpoint)/2;
+//             if(arr[midpoint]==target){
+//                 System.out.println("at index:"+midpoint);}
+//             if (arr[midpoint]>target) {
+//                    endpoint=arr[midpoint-1];
+                
+//             } else {
+//                 startpoint=arr[midpoint+1];
+//             }
+            
+
+            
+//         }
+//     }
+
+// }
+// public class day8{
+//     void bSort(){
+//         for (int i = 0; i<str.length()-1; i++) {
+//             for (int j = 0; j < str.length()-i-1; j++) {
+//                 if((int)str[j].chatAt(0)>(int)str[j+1].charAt(0)){
+
+//                 }          
+//             }
+            
+//         }
+//     }
+//     public static void main(String[] args){
+
+//     }
+// }
+// 
+// class Student{
+//     String name="MONTY";
+//     void sum(int a,int b){
+//         System.out.println( a+b);
+//     }
+// }
+// public class day8{
+//     public static void main(String[] args) {
+        
+    
+//         Student obj=new Student();
+        
+//         obj.sum(5,2);
+        
+//     }
+// }
+
+   
+// class fullname{
+//     void mon{
+//     String firstname="monty";
+//     String lastname="dehariya";
+//     System.out.println( firstname+"  "+lastname);
+// }}
+// class Student{
+//     String name="MONTY";
+//     void sum(int a,int b){
+//         System.out.println( a+b);
+//     }
+// }
+// public class day8{
+//     public static void main(String[] args) {
+        
+    
+//         Student obj=new Student();
+//         fullname obj1=new fullname();
+//         obj.sum(5,2);
+//         obj1.mon();
+//         System.out.println(firstname);
+//         System.out.println(lastname);
+//     }
+// }
